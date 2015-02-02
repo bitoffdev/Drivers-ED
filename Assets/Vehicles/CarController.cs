7e3b712d3bd666b04966e68bf24ff6e750c08387
Elliot <emoney543@gmail.com>
2015-02-02 14:40:41 -0500

    Finished Basic CarController

--- Assets/Vehicles/CarController.cs
+++ Assets/Vehicles/CarController.cs
@@ -4,42 +4,54 @@ using System.Collections;
 public class CarController : MonoBehaviour {
 	public WheelCollider FrontLeftWheel;
 	public WheelCollider FrontRightWheel;
+
 	public float[] GearRatio;
 	private int CurrentGear = 0;
+
 	public float EngineTorque = 230.0f;
 	public float MaxEngineRPM = 3000.0f;
 	public float MinEngineRPM = 1000.0f;
 	private float EngineRPM = 0.0f;
 
+	private float CurrentThrottle = 0f;
+	private float CurrentSteerAngle = 0f;
+
+	public Vector3 MassCenterOffset = Vector3.zero;
+
 	void Start () {
-		rigidbody.centerOfMass += new Vector3(0f, -.75f, .25f);
+		rigidbody.centerOfMass += MassCenterOffset;
 	}
-	
-	// Update is called once per frame
+	/// <summary>
+	/// Sets the throttle.
+	/// </summary>
+	/// <param name="throttle">Should be between -1 and 1</param>
+	public void SetThrottle(float throttle){
+		CurrentThrottle = Mathf.Clamp(throttle, -1f, 1f);
+	}
+	/// <summary>
+	/// Sets the steering angle.
+	/// </summary>
+	/// <param name="angle">Angle</param>
+	public void SetSteerAngle(float angle){
+		CurrentSteerAngle = angle;
+	}
 	void Update () {
 		// Compute the engine RPM based on the average RPM of the two wheels, then call the shift gear function
 		EngineRPM = (FrontLeftWheel.rpm + FrontRightWheel.rpm)/2f * GearRatio[CurrentGear];
 		ShiftGears();
 		
-		// set the audio pitch to the percentage of RPM to the maximum RPM plus one, this makes the sound play
+		// Set the audio pitch to the percentage of RPM to the maximum RPM plus one, this makes the sound play
 		// up to twice it's pitch, where it will suddenly drop when it switches gears.
-		audio.pitch = Mathf.Abs(EngineRPM / MaxEngineRPM) + 1.0f ;
-		// this line is just to ensure that the pitch does not reach a value higher than is desired.
-		if (audio.pitch > 2.0f) {
-			audio.pitch = 2.0f;
-		}
-		
-		// finally, apply the values to the wheels.    The torque applied is divided by the current gear, and
-		// multiplied by the user input variable.
-		FrontLeftWheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * Input.GetAxis("Vertical");
-		FrontRightWheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * Input.GetAxis("Vertical");
-		
-		// the steer angle is an arbitrary value multiplied by the user input.
-		FrontLeftWheel.steerAngle = 10f * Input.GetAxis("Horizontal");
-		FrontRightWheel.steerAngle = 10f * Input.GetAxis("Horizontal");
+		audio.pitch = Mathf.Min (Mathf.Abs(EngineRPM / MaxEngineRPM) + 1.0f, 2f);
+
+		// Apply values to the Wheel controllers
+		FrontLeftWheel.motorTorque = EngineTorque / GearRatio [CurrentGear] * CurrentThrottle;
+		FrontRightWheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * CurrentThrottle;
+		FrontLeftWheel.steerAngle = CurrentSteerAngle;
+		FrontRightWheel.steerAngle = CurrentSteerAngle;
 	}
 	void ShiftGears(){
-		// this funciton shifts the gears of the vehcile, it loops through all the gears, checking which will make
+		// This funciton shifts the gears of the vehcile, it loops through all the gears, checking which will make
 		// the engine RPM fall within the desired range. The gear is then set to this "appropriate" value.
 		if ( EngineRPM >= MaxEngineRPM ) {
 			int AppropriateGear = CurrentGear;
---
+++ Assets/Vehicles/PlayerCar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CarController))]
+public class PlayerCar : MonoBehaviour {
+	CarController controller;
+
+	void Start () {
+		controller = gameObject.GetComponent<CarController> ();
+	}
+	
+	void Update () {
+		controller.SetThrottle (Input.GetAxis("Vertical"));
+		controller.SetSteerAngle (20f * Input.GetAxis("Horizontal"));
+	}
+}
