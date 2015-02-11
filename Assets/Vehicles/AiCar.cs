9a2eecb7257f919fc81a01fb52882487cced72fc
Ethan <willetts.ethan@wcsstudents.com>
2015-02-11 10:32:49 -0500

    new files

---
+++ Assets/Vehicles/AiCar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CarController))]
+public class PlayerCar : MonoBehaviour {
+	CarController controller;
+	float SteerAngle;
+
+	void Start () {
+		controller = gameObject.GetComponent<CarController> ();
+	}
+	
+	void Update () {
+		controller.SetAcceleration (Random.Range(-1, 1));
+		controller.SetSteering (Random.Range(-1, 1));
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect(10f, 10f, 200f, 20f), string.Format("RPM: {0:0.00}, Speed: {1:0.00} MPH", controller.GetRPM(), rigidbody.velocity.magnitude*2.2369f));
+	}
+}
