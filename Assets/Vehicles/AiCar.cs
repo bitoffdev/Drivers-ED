07df297d76f541da8f19cb3275413b11c722d5d8
Ethan <willetts.ethan@wcsstudents.com>
2015-02-13 14:19:17 -0500

    Made it even better

--- Assets/Vehicles/AiCar.cs
+++ Assets/Vehicles/AiCar.cs
@@ -2,20 +2,23 @@
 using System.Collections;
 
 [RequireComponent(typeof(CarController))]
-public class PlayerCar : MonoBehaviour {
+public class AiCar : MonoBehaviour {
 	CarController controller;
 	float SteerAngle;
 
 	void Start () {
 		controller = gameObject.GetComponent<CarController> ();
+		// We need to make SteerAngle be able to range from 0 degrees to 360 degrees.
+		SteerAngle = Random.Range (-1, 1);
+		controller.SetSteering (SteerAngle);
 	}
 	
 	void Update () {
-		controller.SetAcceleration (Random.Range(-1, 1));
-		controller.SetSteering (Random.Range(-1, 1));
+		controller.SetAcceleration (Random.Range (0, 1));
+
 	}
 
-	void OnGUI(){
+	/*void OnGUI(){
 		GUI.Label (new Rect(10f, 10f, 200f, 20f), string.Format("RPM: {0:0.00}, Speed: {1:0.00} MPH", controller.GetRPM(), rigidbody.velocity.magnitude*2.2369f));
-	}
+	}*/
 }
